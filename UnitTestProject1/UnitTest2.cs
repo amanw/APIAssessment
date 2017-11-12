98608d261de27f489b57c85ca5938dbfb50cebbb
Aman Wangde <aman.wangde@gmail.com>
2017-11-11 22:15:48 -0500

    Add files via upload

---
+++ UnitTestProject1/UnitTest2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProgramAPIAssignment;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestCheckDateTrue()
+        {
+            var actual = ProgramAPIAssignment.ProgramAPIAssignmentObjectcs.CheckDate("2017-05-06", "2017-05-07");
+            Assert.AreEqual(true, actual, "Expected value to be true");
+        }
+
+        [TestMethod]
+        public void TestCheckDateFalse()
+        {
+            var actual = ProgramAPIAssignment.ProgramAPIAssignmentObjectcs.CheckDate("2017-05-06", "2017-05-09");
+            Assert.AreEqual(false, actual, "Expected value to be true");
+        }
+
+        [TestMethod]
+        public void TestCheckDateEmpty()
+        {
+            var actual = ProgramAPIAssignment.ProgramAPIAssignmentObjectcs.CheckDate("", "2017-05-09");
+            Assert.AreEqual(false, actual, "Expected value to be true");
+        }
+    }
+}
